c5bc90b4c2608f3d60d5b50992c99e29bbab72fe
codywashbrook <codywashbrook@hotmail.com>
2022-10-25 22:09:27 -0300

    added arrays to methods

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -8,6 +8,9 @@ namespace Arrays
 {
     internal class Program
     {
+        static int weapon;
+        static string[] weaponName;
+        static int[] ammo;
         static void Main(string[] args)
         {
             Console.WriteLine("Arrays");
@@ -18,10 +21,8 @@ namespace Arrays
             //2 rocket launcher
 
             //current weapon
-            int weapon; // declaration
             weapon = 0; //0...2 init
 
-            string[] weaponName;
             weaponName = new string[3];
             weaponName[0] = "Pistol";
             weaponName[1] = "Shotgun";
@@ -33,29 +34,62 @@ namespace Arrays
 
             // 
 
-            int[] ammo; //array of integers; declaration
+            //int[] ammo; //array of integers; declaration
             ammo = new int[3]; // instantiation
             ammo[0] = 6; //pistol declaration ..
             ammo[1] = 2; // shotgun
             ammo[2] = 3; //rocket launcher
 
+
+
             //show cuurent weapon
-            Console.WriteLine(weaponName[weapon]); // shows current weapon
-            weapon = 1;
-            Console.WriteLine(weaponName[weapon]);
-            weapon = 2;
-            Console.WriteLine(weaponName[weapon]);
+            //Console.WriteLine(weaponName[weapon]);// shows current weapon
+            //Console.WriteLine(ammo[weapon]); // shows current ammo
+            //ShowHUD();
+            //weapon = 1;
+            //ShowHUD();
+            //weapon = 2;
+            //ShowHUD();
+            //weapon = 3;
+            CurrentWeapon(0);
+            Fire(2);
+            ShowHUD();
+
+
 
             Console.ReadKey(true);
         }
-        static void ShowCurrentWeaponAmmo()
+        static void ShowHUD()
         {
-
+            Console.WriteLine(weaponName[weapon]); // shows current weapon
+            Console.WriteLine(ammo[weapon]);
         }
 
-        static void Fire()
+        static void Fire(int shoot)
         {
 
+            ammo[weapon] = ammo[weapon] - shoot;
+        }
+
+        static void Reload(int relAmmo)
+        {
+            ammo[weapon] = ammo[weapon] + relAmmo;
+        }
+
+        static void CurrentWeapon(int currWeap)
+        {
+            if (currWeap == 0)
+            {
+                weapon = currWeap;
+            }
+            if (currWeap == 1)
+            {
+                weapon = currWeap;
+            }
+            if (currWeap == 2)
+            {
+                weapon = currWeap;
+            }
         }
     }
 }
